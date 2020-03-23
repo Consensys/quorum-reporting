c643842dc675126a65eeb9da8312db5a8bf78211
vsmk98 <vsaimk@gmail.com>
2020-03-23 17:18:44 +0800

    Changes to read details form config file and pass.

--- core/backend.go
+++ core/backend.go
@@ -2,6 +2,9 @@ package core
 
 import (
 	"fmt"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/hpcloud/tail/util"
+	"github.com/naoina/toml"
 	"os"
 	"os/signal"
 	"syscall"
@@ -11,8 +14,19 @@ import (
 	"quorumengineering/quorum-report/core/monitor"
 	"quorumengineering/quorum-report/core/rpc"
 	"quorumengineering/quorum-report/database"
-	"quorumengineering/quorum-report/types"
 )
+
+type ReportInputStruct struct {
+	Title     string
+	Reporting struct {
+		WSUrl       string
+		GraphQLUrl  string
+		Addresses   []common.Address
+		RPCAddr     string
+		RPCCorsList []string
+		RPCVHosts   []string
+	}
+}
 
 // Backend wraps MonitorService and QuorumClient, controls the start/stop of the reporting tool.
 type Backend struct {
@@ -21,16 +35,29 @@ type Backend struct {
 	rpc     *rpc.RPCService
 }
 
-func New(flags *types.Flags) (*Backend, error) {
-	quorumClient, err := client.NewQuorumClient(flags.QuorumWSURL, flags.QuorumGraphQLURL)
+func ReadConfig(configFile string) ReportInputStruct {
+	f, err := os.Open(configFile)
+	if err != nil {
+		util.Fatal("unable to open the config file %v", err)
+	}
+	defer f.Close()
+	var input ReportInputStruct
+	if err := toml.NewDecoder(f).Decode(&input); err != nil {
+		util.Fatal("unable to open the config file %v", err)
+	}
+	return input
+}
+
+func New(config ReportInputStruct) (*Backend, error) {
+	quorumClient, err := client.NewQuorumClient(config.Reporting.WSUrl, config.Reporting.GraphQLUrl)
 	if err != nil {
 		return nil, err
 	}
 	db := database.NewMemoryDB()
 	return &Backend{
 		monitor: monitor.NewMonitorService(db, quorumClient),
-		filter:  filter.NewFilterService(db, flags.Addresses),
-		rpc:     rpc.NewRPCService(db, flags.RPCAddress, flags.RPCVHOSTS, flags.RPCCORS),
+		filter:  filter.NewFilterService(db, config.Reporting.Addresses),
+		rpc:     rpc.NewRPCService(db, config.Reporting.RPCAddr, config.Reporting.RPCVHosts, config.Reporting.RPCCorsList),
 	}, nil
 }
 
--- main.go
+++ main.go
@@ -2,49 +2,27 @@ package main
 
 import (
 	"flag"
+	"github.com/hpcloud/tail/util"
 	"log"
-	"strings"
-
-	"github.com/ethereum/go-ethereum/common"
-
 	"quorumengineering/quorum-report/core"
-	"quorumengineering/quorum-report/types"
 )
 
 func main() {
-	// Define flags with default values.
-	quorumWSURL := flag.String("quorumWSURL", "ws://localhost:23000", "WebSocket URL to quorum node")
-	quorumGraphQLURL := flag.String("quorumGraphQLURL", "http://localhost:8547/graphql", "GraphQL URL to quorum node")
-	addresses := flag.String("addresses", "", "Common separated hex contract addresses")
-	rpcAddr := flag.String("rpcaddr", "localhost:6666", "HTTP-RPC server listening interface")
-	rpccors := flag.String("rpccors", "localhost", "Comma separated list of virtual hostnames from which to accept requests (server enforced). Accepts '*' wildcard.")
-	rpcvhosts := flag.String("rpcvhosts", "localhost", "Common separated hex contract addresses")
-	// Once all flags are declared, call flag.Parse() to execute the command-line parsing.
+	// expects one input which the config file
+	// read the config file path
+	var configFile string
+	flag.StringVar(&configFile, "config", "", "config file")
 	flag.Parse()
 
-	// Process all flags to make them into a single `flags` struct.
-	addressList := []common.Address{}
-	for _, a := range strings.Split(*addresses, ",") {
-		addressList = append(addressList, common.HexToAddress(a))
-	}
-	rpccorsList := []string{}
-	for _, h := range strings.Split(*rpccors, ",") {
-		rpccorsList = append(rpccorsList, h)
-	}
-	rpcvhostsList := []string{}
-	for _, h := range strings.Split(*rpcvhosts, ",") {
-		rpcvhostsList = append(rpcvhostsList, h)
-	}
-	flags := &types.Flags{
-		QuorumWSURL:      *quorumWSURL,
-		QuorumGraphQLURL: *quorumGraphQLURL,
-		RPCAddress:       *rpcAddr,
-		RPCCORS:          rpccorsList,
-		RPCVHOSTS:        rpcvhostsList,
-		Addresses:        addressList,
+	if configFile == "" {
+		util.Fatal("config file path not given. cannot start the service")
 	}
 
-	backend, err := core.New(flags)
+	// read the given config file
+	config := core.ReadConfig(configFile)
+
+	// start the back end with given config
+	backend, err := core.New(config)
 	if err != nil {
 		log.Fatalf("exiting... err: %v.\n", err)
 		return
