2b637b05038b50342b1e140a6f1f3cce919c0b16
Peter <peter.rd.fox@gmail.com>
2020-05-12 22:23:57 +0100

    Add godocs
    Add godocs for interface methods.

--- client/interface.go
+++ client/interface.go
@@ -12,8 +12,11 @@ type Client interface {
 	// from ethclient.Client
 	SubscribeNewHead(context.Context, chan<- *ethTypes.Header) (ethereum.Subscription, error)
 	BlockByNumber(context.Context, *big.Int) (*ethTypes.Block, error)
-	// graphql
+
+	// ExecuteGraphQLQuery performs a fully constructed query against the Geth
+	// GraphQL server
 	ExecuteGraphQLQuery(context.Context, interface{}, string) error
-	// rpc
+
+	// RPCCall makes an arbitrary call to the Geth RPC server
 	RPCCall(context.Context, interface{}, string, ...interface{}) error
 }
