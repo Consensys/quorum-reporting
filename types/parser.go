fae7c15b843b5f5db75001dc55c6fb0bbc1b23cb
zzy96 <zhou0250@e.ntu.edu.sg>
2020-05-28 21:07:40 +0800

    parse constructor based on bytecode meta data

--- types/parser.go
+++ types/parser.go
@@ -46,9 +46,19 @@ func (ptx *ParsedTransaction) ParseTransaction(rawABI string) error {
 	} else {
 		// contract deployment transaction
 		ptx.Sig = "constructor" + parsedABI.Constructor.Sig()
-		if len(data) > 32*parsedABI.Constructor.Inputs.LengthNonIndexed() {
-			parsedABI.Constructor.Inputs.UnpackIntoMap(ptx.ParsedData, data[(len(data)-32*parsedABI.Constructor.Inputs.LengthNonIndexed()):])
-			// TODO: Parsing inputs for complex data type in constructor is not supported unless the exact contract bin is provided.
+		dataHex := hexutil.Encode(data)
+		if index := strings.Index(dataHex, "a165627a7a72305820"); index > 0 {
+			// search for pattern a165627a7a72305820 for solidity < 0.5.10
+			// <bytecode> + "a165627a7a72305820" + <256 bits whisperHash> + "0029"
+			index = (index - 2 + 18 + 64 + 4) / 2 // remove 0x, find hex position 18+64+4 after
+			parsedABI.Constructor.Inputs.UnpackIntoMap(ptx.ParsedData, data[index:])
+		} else if index := strings.Index(dataHex, "64736f6c6343"); index > 0 {
+			// search for pattern 64736f6c6343 for solidity >= 0.5.10,
+			// <bytecode> + "a265627a7a72305820" + <256 bits whisperHash> + "64736f6c6343" + compiler_version(e.g. 000608) + "0033"
+			index = (index - 2 + 12 + 6 + 4) / 2 // remove 0x, find hex position 12+6+4 after
+			parsedABI.Constructor.Inputs.UnpackIntoMap(ptx.ParsedData, data[index:])
+		} else {
+			ptx.ParsedData["error"] = "unable to parse params"
 		}
 	}
 	return nil
