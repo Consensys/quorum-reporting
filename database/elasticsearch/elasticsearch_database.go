8478cd434f5ff7515925934aab1c8f8b634103a0
Peter <peter.rd.fox@gmail.com>
2020-07-02 09:47:05 +0100

    Fix import order

--- database/elasticsearch/elasticsearch_database.go
+++ database/elasticsearch/elasticsearch_database.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"quorumengineering/quorum-report/database"
 	"strconv"
 	"strings"
 	"sync"
@@ -15,6 +14,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/state"
 
+	"quorumengineering/quorum-report/database"
 	"quorumengineering/quorum-report/log"
 	"quorumengineering/quorum-report/types"
 )
