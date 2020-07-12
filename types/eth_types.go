67011933d8865f34ae1d23ed95a3016608857ebb
Peter <peter.rd.fox@gmail.com>
2020-07-12 21:34:55 +0100

    Switch internal call "From" field

--- types/eth_types.go
+++ types/eth_types.go
@@ -7,6 +7,11 @@ import (
 	"strings"
 )
 
+var (
+	EmptyHash    = NewHash("")
+	EmptyAddress = NewAddress("")
+)
+
 type Address string
 
 // NewAddressFromHex creates a new address from a given hex string
