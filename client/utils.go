60d9911420c194c329909cdbdba71ac8886f3073
Peter <peter.rd.fox@gmail.com>
2020-08-05 14:35:55 +0100

    Remove unused function

--- client/utils.go
+++ client/utils.go
@@ -140,17 +140,3 @@ func CallBalanceOfERC20(c Client, contract types.Address, holder types.Address, 
 	err := c.RPCCall(&res, "eth_call", msg, fmt.Sprintf("0x%x", blockNum))
 	return res, err
 }
-
-func CallBalanceOfERC721(c Client, contract types.Address, holder types.Address, blockNum uint64) (types.HexData, error) {
-	// 70a08231 is the 4byte function sig
-	// "000000000000000000000000" + string(holder) is the token holders address, padded to 32 bytes
-
-	msg := types.EIP165Call{
-		To:   contract,
-		Data: types.NewHexData("0x70a08231" + "000000000000000000000000" + string(holder)),
-	}
-
-	var res types.HexData
-	err := c.RPCCall(&res, "eth_call", msg, fmt.Sprintf("0x%x", blockNum))
-	return res, err
-}
