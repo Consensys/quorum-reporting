cbbc467ad8d964039fc279a22305a95f7210a7ba
Peter <peter.rd.fox@gmail.com>
2020-08-10 17:46:23 +0100

    Update ERC20 recording tests to account for searching of previous token balance

--- database/elasticsearch/elasticsearch_token_database_test.go
+++ database/elasticsearch/elasticsearch_token_database_test.go
@@ -1,7 +1,6 @@
 package elasticsearch
 
 import (
-	"errors"
 	"math/big"
 	"strings"
 	"testing"
@@ -15,7 +14,7 @@ import (
 	"quorumengineering/quorum-report/types"
 )
 
-func TestElasticsearchDB_RecordNewERC20Balance(t *testing.T) {
+func TestElasticsearchDB_RecordNewERC20Balance_NoPrevious(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
@@ -32,14 +31,43 @@ func TestElasticsearchDB_RecordNewERC20Balance(t *testing.T) {
 		BlockNumber: blockNumber,
 		Amount:      balance.String(),
 	}
-
 	ex := esapi.IndexRequest{
 		Index:      ERC20TokenIndex,
 		DocumentID: "0x1932c48b2bf8102ba33b4a6b545c32236e342f34-0x1349f3e1b8d71effb47b840594ff27da7e603d17-10",
 		Body:       esutil.NewJSONReader(token),
 	}
 
+	searchQuery := `
+{
+	"query": {
+		"bool": {
+			"must": [
+				{ "match": { "contract": "0x1932c48b2bf8102ba33b4a6b545c32236e342f34"} },
+				{ "match": { "holder": "0x1349f3e1b8d71effb47b840594ff27da7e603d17" } },
+				{ "range": { "blockNumber": { "lte": 9 } } }
+			]
+		}
+	},
+	"sort": [
+			{
+				"blockNumber": {
+					"order": "desc",
+					"unmapped_type": "long"
+				}
+			}
+	]
+}
+`
+	size := 1
+	req := esapi.SearchRequest{
+		Index: []string{ERC20TokenIndex},
+		Body:  strings.NewReader(searchQuery),
+		Size:  &size,
+	}
+	searchResult := `{"hits": {"hits": []}}`
+
 	mockedClient.EXPECT().DoRequest(gomock.Any()) //for setup, not relevant to test
+	mockedClient.EXPECT().DoRequest(NewSearchRequestMatcher(req)).Return([]byte(searchResult), nil)
 	mockedClient.EXPECT().DoRequest(NewIndexRequestMatcher(ex)).Do(func(input esapi.IndexRequest) {
 		assert.Equal(t, "create", input.OpType)
 	})
@@ -49,7 +77,7 @@ func TestElasticsearchDB_RecordNewERC20Balance(t *testing.T) {
 	assert.Nil(t, err, "expected error to be nil")
 }
 
-func TestElasticsearchDB_RecordNewERC20Balance_WithClientError(t *testing.T) {
+func TestElasticsearchDB_RecordNewERC20Balance_WithPrevious(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
@@ -66,21 +94,66 @@ func TestElasticsearchDB_RecordNewERC20Balance_WithClientError(t *testing.T) {
 		BlockNumber: blockNumber,
 		Amount:      balance.String(),
 	}
-
 	ex := esapi.IndexRequest{
 		Index:      ERC20TokenIndex,
 		DocumentID: "0x1932c48b2bf8102ba33b4a6b545c32236e342f34-0x1349f3e1b8d71effb47b840594ff27da7e603d17-10",
 		Body:       esutil.NewJSONReader(token),
 	}
 
+	searchQuery := `
+{
+	"query": {
+		"bool": {
+			"must": [
+				{ "match": { "contract": "0x1932c48b2bf8102ba33b4a6b545c32236e342f34"} },
+				{ "match": { "holder": "0x1349f3e1b8d71effb47b840594ff27da7e603d17" } },
+				{ "range": { "blockNumber": { "lte": 9 } } }
+			]
+		}
+	},
+	"sort": [
+			{
+				"blockNumber": {
+					"order": "desc",
+					"unmapped_type": "long"
+				}
+			}
+	]
+}
+`
+	size := 1
+	req := esapi.SearchRequest{
+		Index: []string{ERC20TokenIndex},
+		Body:  strings.NewReader(searchQuery),
+		Size:  &size,
+	}
+	searchResult := `{"hits": {"hits": [
+{"_source": {
+		"contract": "0x1932c48b2bf8102ba33b4a6b545c32236e342f34",
+		"holder": "0x1349f3e1b8d71effb47b840594ff27da7e603d17",
+		"token": "500",
+		"blockNumber": 7
+	}
+}
+]}}`
+
+	oldTokenUpdateReq := esapi.UpdateRequest{
+		Index:      ERC20TokenIndex,
+		DocumentID: "0x1932c48b2bf8102ba33b4a6b545c32236e342f34-0x1349f3e1b8d71effb47b840594ff27da7e603d17-7",
+		Body: strings.NewReader(`{"doc":{"heldUntil":9}}
+`),
+	}
+
 	mockedClient.EXPECT().DoRequest(gomock.Any()) //for setup, not relevant to test
+	mockedClient.EXPECT().DoRequest(NewSearchRequestMatcher(req)).Return([]byte(searchResult), nil)
 	mockedClient.EXPECT().DoRequest(NewIndexRequestMatcher(ex)).Do(func(input esapi.IndexRequest) {
 		assert.Equal(t, "create", input.OpType)
-	}).Return(nil, errors.New("test error"))
+	})
+	mockedClient.EXPECT().DoRequest(NewUpdateRequestMatcher(oldTokenUpdateReq)).Return(nil, nil)
 
 	db, _ := New(mockedClient)
 	err := db.RecordNewERC20Balance(tokenContractAddress, holderAddress, blockNumber, balance)
-	assert.EqualError(t, err, "test error")
+	assert.Nil(t, err, "expected error to be nil")
 }
 
 func TestElasticsearchDB_GetERC20Balance_PaginationTooLarge(t *testing.T) {
@@ -91,7 +164,7 @@ func TestElasticsearchDB_GetERC20Balance_PaginationTooLarge(t *testing.T) {
 
 	tokenContractAddress := types.NewAddress("0x1932c48b2bf8102ba33b4a6b545c32236e342f34")
 	holderAddress := types.NewAddress("0x1349f3e1b8d71effb47b840594ff27da7e603d17")
-	options := &types.QueryOptions{
+	options := &types.TokenQueryOptions{
 		PageSize:   100,
 		PageNumber: 11,
 	}
@@ -111,20 +184,45 @@ func TestElasticsearchDB_GetERC20Balance_NoResults(t *testing.T) {
 
 	tokenContractAddress := types.NewAddress("0x1932c48b2bf8102ba33b4a6b545c32236e342f34")
 	holderAddress := types.NewAddress("0x1349f3e1b8d71effb47b840594ff27da7e603d17")
-	options := &types.QueryOptions{}
+	options := &types.TokenQueryOptions{}
 	options.SetDefaults()
 
 	expectedQuery := `
 {
-	"query": {
-		"bool": {
-			"must": [
-				{ "match": { "contract": "0x1932c48b2bf8102ba33b4a6b545c32236e342f34"} },
-				{ "match": { "holder": "0x1349f3e1b8d71effb47b840594ff27da7e603d17" } },
-{ "range": { "blockNumber": { "gte": 0 } } }
-			]
-		}
-	}
+  "query": {
+    "bool": {
+
+      "filter": [
+        {
+          "bool": {
+            "should": [
+              { "range": { "blockNumber": { "gte": 0 } } },
+              {
+                "bool": {
+                  "must": [{"range": {"blockNumber": {"lt": 0}}}],
+                  "filter": [
+                    {
+                      "bool": {
+                        "should": [
+                          {"range": {"heldUntil": {"gte": 0}}},
+                          {"bool": {"must_not": {"exists": {"field": "heldUntil"}}}}
+                        ]
+                      }
+                    }
+                  ]
+                }
+              }
+            ]
+          }
+        }
+      ],
+
+      "must": [
+        {"match": {"contract": "0x1932c48b2bf8102ba33b4a6b545c32236e342f34"}},
+        {"match": {"holder": "0x1349f3e1b8d71effb47b840594ff27da7e603d17"}}
+      ]
+    }
+  }
 }
 `
 
@@ -157,20 +255,45 @@ func TestElasticsearchDB_GetERC20Balance_MultipleResults(t *testing.T) {
 
 	tokenContractAddress := types.NewAddress("0x1932c48b2bf8102ba33b4a6b545c32236e342f34")
 	holderAddress := types.NewAddress("0x1349f3e1b8d71effb47b840594ff27da7e603d17")
-	options := &types.QueryOptions{}
+	options := &types.TokenQueryOptions{}
 	options.SetDefaults()
 
 	expectedQuery := `
 {
-	"query": {
-		"bool": {
-			"must": [
-				{ "match": { "contract": "0x1932c48b2bf8102ba33b4a6b545c32236e342f34"} },
-				{ "match": { "holder": "0x1349f3e1b8d71effb47b840594ff27da7e603d17" } },
-{ "range": { "blockNumber": { "gte": 0 } } }
-			]
-		}
-	}
+  "query": {
+    "bool": {
+
+      "filter": [
+        {
+          "bool": {
+            "should": [
+              { "range": { "blockNumber": { "gte": 0 } } },
+              {
+                "bool": {
+                  "must": [{"range": {"blockNumber": {"lt": 0}}}],
+                  "filter": [
+                    {
+                      "bool": {
+                        "should": [
+                          {"range": {"heldUntil": {"gte": 0}}},
+                          {"bool": {"must_not": {"exists": {"field": "heldUntil"}}}}
+                        ]
+                      }
+                    }
+                  ]
+                }
+              }
+            ]
+          }
+        }
+      ],
+
+      "must": [
+        {"match": {"contract": "0x1932c48b2bf8102ba33b4a6b545c32236e342f34"}},
+        {"match": {"holder": "0x1349f3e1b8d71effb47b840594ff27da7e603d17"}}
+      ]
+    }
+  }
 }
 `
 
@@ -199,6 +322,82 @@ func TestElasticsearchDB_GetERC20Balance_MultipleResults(t *testing.T) {
 	assert.Len(t, results, 2)
 	assert.EqualValues(t, 500, results[1].Int64())
 	assert.EqualValues(t, 2000, results[2].Int64())
+}
+
+func TestElasticsearchDB_GetERC20Balance_ResultBeforeBeginBlock(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockedClient := elasticsearchmocks.NewMockAPIClient(ctrl)
+
+	tokenContractAddress := types.NewAddress("0x1932c48b2bf8102ba33b4a6b545c32236e342f34")
+	holderAddress := types.NewAddress("0x1349f3e1b8d71effb47b840594ff27da7e603d17")
+	options := &types.TokenQueryOptions{}
+	options.BeginBlockNumber = big.NewInt(1)
+	options.SetDefaults()
+
+	expectedQuery := `
+{
+  "query": {
+    "bool": {
+
+      "filter": [
+        {
+          "bool": {
+            "should": [
+              { "range": { "blockNumber": { "gte": 1 } } },
+              {
+                "bool": {
+                  "must": [{"range": {"blockNumber": {"lt": 1}}}],
+                  "filter": [
+                    {
+                      "bool": {
+                        "should": [
+                          {"range": {"heldUntil": {"gte": 1}}},
+                          {"bool": {"must_not": {"exists": {"field": "heldUntil"}}}}
+                        ]
+                      }
+                    }
+                  ]
+                }
+              }
+            ]
+          }
+        }
+      ],
+
+      "must": [
+        {"match": {"contract": "0x1932c48b2bf8102ba33b4a6b545c32236e342f34"}},
+        {"match": {"holder": "0x1349f3e1b8d71effb47b840594ff27da7e603d17"}}
+      ]
+    }
+  }
+}
+`
+
+	from := 0
+	size := 10
+	req := esapi.SearchRequest{
+		Index: []string{ERC20TokenIndex},
+		Body:  strings.NewReader(expectedQuery),
+		From:  &from,
+		Size:  &size,
+		Sort:  []string{"blockNumber:desc"},
+	}
+
+	result := `{"hits": {"hits": [
+  {"_source": {"blockNumber": 0, "amount": "500"}}
+]}}`
+
+	mockedClient.EXPECT().DoRequest(gomock.Any()) //for setup, not relevant to test
+	mockedClient.EXPECT().DoRequest(NewSearchRequestMatcher(req)).Return([]byte(result), nil)
+
+	db, _ := New(mockedClient)
+	results, err := db.GetERC20Balance(tokenContractAddress, holderAddress, options)
+
+	assert.Nil(t, err)
+	assert.Len(t, results, 2)
+	assert.EqualValues(t, 500, results[0].Int64())
+	assert.EqualValues(t, 500, results[1].Int64())
 }
 
 func TestElasticsearchDB_ERC721TokenByTokenID_NotFound(t *testing.T) {
