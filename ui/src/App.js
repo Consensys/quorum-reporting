96db33f3839850117e69e7906acd3799b220f0e1
Brad McDermott <bradmcdermott@gmail.com>
2020-09-17 14:38:06 -0400

    Waits to show UI until connected to backend, to fix weird starting state issues

--- ui/src/App.js
+++ ui/src/App.js
@@ -6,24 +6,31 @@ import { BlockDetail } from './containers/BlockDetail'
 import { TransactionDetail } from './containers/TransactionDetail'
 import { ContractDetail } from './containers/ContractDetail'
 import ContractListContainer from './containers/ContractListContainer'
+import { useSelector } from 'react-redux'
+import CircularProgress from '@material-ui/core/CircularProgress'
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
+  },
+  loading: {
+    margin: 24,
   }
-
 }))
 
 function App () {
   const classes = useStyles()
+  const ready = useSelector(state => state.system.isConnected && state.system.lastPersistedBlockNumber)
   return (
     <Router>
       <div className={classes.root}>
         <HeaderContainer/>
         {/* A <Switch> looks through its children <Route>s and
               renders the first one that matches the current URL. */}
+        {!ready && <CircularProgress className={classes.loading}/>}
+        {ready &&
         <Switch>
           <Route path="/blocks/:id" render={({ match }) => <BlockDetail number={match.params.id}/>}/>
           <Route path="/transactions/:id" render={({ match }) => <TransactionDetail id={match.params.id}/>}/>
@@ -33,6 +40,7 @@ function App () {
             <ContractListContainer/>
           </Route>
         </Switch>
+        }
       </div>
     </Router>
   )
